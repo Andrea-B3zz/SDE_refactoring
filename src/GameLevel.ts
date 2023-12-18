69b765b752180020614d6068e6c20556f657f65f
Andrea Bezzolato <bezz0001@hz.nl>
2023-12-18 14:04:30 +0100

    Hiboxes updated

--- src/GameLevel.ts
+++ src/GameLevel.ts
@@ -27,42 +27,35 @@ export default class GameLevel extends Level {
 
     this.canvas = canvas;
     this.populateWalls();
-    canvas.style.marginLeft="";
-    canvas.style.width="";
-    canvas.style.height="";
+    canvas.style.marginLeft = "";
+    canvas.style.width = "";
+    canvas.style.height = "";
     this.image = CanvasRenderer.loadNewImage('./assets/FinalMap.png');
 
     this.mouseListener = new MouseListener(this.canvas);
   }
 
   private populateWalls(): void {
-    this.walls.push(new Wall(192, 203, 16, 182));
-    this.walls.push(new Wall(410, 422, 69, 185));
-    this.walls.push(new Wall(614, 626, 17, 490,));
-    this.walls.push(new Wall(769, 780, 17, 106));
-    this.walls.push(new Wall(998, 1011, 17, 94));
-    this.walls.push(new Wall(1102, 1114, 82, 185));
-    this.walls.push(new Wall(1242, 1254, 17, 185));
-    this.walls.push(new Wall(295, 307, 210, 361));
-    this.walls.push(new Wall(769, 781, 183, 260));
-    this.walls.push(new Wall(806, 818, 400, 488));
-    this.walls.push(new Wall(1254, 1266, 325, 413));
-    this.walls.push(new Wall(257, 268, 461, 579));
-    this.walls.push(new Wall(257, 267, 643, 735));
-    this.walls.push(new Wall(718, 730, 477, 645));
-    this.walls.push(new Wall(922, 934, 477, 644));
-    this.walls.push(new Wall(1267, 1279, 477, 733));
-    this.walls.push(new Wall(1395, 1408, 477, 580));
-    this.walls.push(new Wall(410, 1012, 171, 184));
-    this.walls.push(new Wall(1102, 1406, 172, 185));
-    this.walls.push(new Wall(193, 946, 350, 362));
-    this.walls.push(new Wall(1267, 1510, 325, 338));
-    this.walls.push(new Wall(1269, 1395, 399, 413));
-    this.walls.push(new Wall(615, 1114, 477, 489));
-    this.walls.push(new Wall(102, 268, 464, 476));
-    this.walls.push(new Wall(474, 754, 630, 644));
-    this.walls.push(new Wall(897, 1113, 630, 896));
-    this.walls.push(new Wall(1267, 1406, 477, 489));
+    this.walls.push(new Wall(151, 154, 144, 251));
+    this.walls.push(new Wall(10, 154, 235, 251));
+    this.walls.push(new Wall(341, 346, 203, 320));
+    this.walls.push(new Wall(341, 554, 203, 219));
+    this.walls.push(new Wall(549, 554, 128, 320));
+    this.walls.push(new Wall(757, 762, 10, 219));
+    this.walls.push(new Wall(757, 864, 203, 219));
+    this.walls.push(new Wall(1030, 1034, 10, 128));
+    this.walls.push(new Wall(10, 154, 427, 443));
+    this.walls.push(new Wall(150, 154, 427, 448));
+    this.walls.push(new Wall(148, 154, 608, 710));
+    this.walls.push(new Wall(336, 586, 507, 523));
+    this.walls.push(new Wall(582, 586, 507, 710));
+    this.walls.push(new Wall(853, 858, 427, 544));
+    this.walls.push(new Wall(853, 1270, 427, 443));
+    this.walls.push(new Wall(1029, 1034, 336, 443));
+    this.walls.push(new Wall(0, 1280, 0, 27));
+    this.walls.push(new Wall(1269, 1280, 0, 720));
+    this.walls.push(new Wall(0, 1280, 699, 720));
+    this.walls.push(new Wall(0, 10, 0, 720));
   }
 
   /**
@@ -82,7 +75,7 @@ export default class GameLevel extends Level {
     }
 
 
-    if(this.mouseListener.isButtonDown(MouseListener.BUTTON_LEFT)){
+    if (this.mouseListener.isButtonDown(MouseListener.BUTTON_LEFT)) {
       console.log(this.mouseListener.getMousePosition().x);
       console.log(this.mouseListener.getMousePosition().y);
     }
@@ -115,13 +108,13 @@ export default class GameLevel extends Level {
 
     const tempW: number = Number(this.canvas.style.width.substring(0, 4));
     const marginWidth: number = (windowWidth - tempW) / 2;
-    const marginPercentW: number = marginWidth*100/windowWidth;
-    // this.canvas.style.marginLeft = marginPercentW + '%';
+    const marginPercentW: number = marginWidth * 100 / windowWidth;
+    this.canvas.style.marginLeft = marginPercentW + '%';
 
     const tempH: number = Number(this.canvas.style.height.substring(0, 3));
     const marginHeight: number = (windowHeight - tempH) / 2;
-    const marginPercentH: number = marginHeight*100/windowHeight;
-    // this.canvas.style.marginTop = marginPercentH + '%';
+    const marginPercentH: number = marginHeight * 100 / windowHeight;
+    this.canvas.style.marginTop = marginPercentH + '%';
 
     /*console.log(tempW);
     console.log(windowWidth);
@@ -135,7 +128,7 @@ export default class GameLevel extends Level {
       const width: number = this.walls[i].getRightX() - this.walls[i].getLeftX();
       const height: number = this.walls[i].getBottomY() - this.walls[i].getTopY();
       CanvasRenderer.drawRectangle(
-        canvas,
+        this.canvas,
         this.walls[i].getLeftX(),
         this.walls[i].getTopY(),
         width,
@@ -143,6 +136,6 @@ export default class GameLevel extends Level {
         this.walls[i].getColor(),
       );
     }
-    this.player.render(canvas);
+    this.player.render(this.canvas);
   }
 }
