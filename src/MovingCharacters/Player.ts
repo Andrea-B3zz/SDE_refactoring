dda78da879c7e49e784a9541a3fb3fba58daad5c
vlad0004p <vlad0004@hz.nl>
2024-01-08 09:58:50 +0200

    added the best friend on the screen

--- src/MovingCharacters/Player.ts
+++ src/MovingCharacters/Player.ts
@@ -11,6 +11,8 @@ export default class Player extends MovingCharacter {
 
   private fovImage: HTMLImageElement;
 
+  private bfImage: HTMLImageElement;
+
   public constructor(walls: Wall[], monsters: Monster[]) {
     super();
     this.walls = walls;
@@ -27,6 +29,8 @@ export default class Player extends MovingCharacter {
     this.posX = 200;
     this.posY = 200;
 
+    this.bfImage = CanvasRenderer.loadNewImage('./assets/bestFriend.png');
+    console.log(this.bfImage);
     this.speed = 0.4;
   }
 
@@ -137,5 +141,7 @@ export default class Player extends MovingCharacter {
       this.posY - this.fovImage.height / 2 + this.getHeight() / 2);
 
     CanvasRenderer.drawImage(canvas, this.image, this.posX, this.posY);
+
+    CanvasRenderer.drawImage(canvas, this.bfImage, 1450, 600);
   }
 }
